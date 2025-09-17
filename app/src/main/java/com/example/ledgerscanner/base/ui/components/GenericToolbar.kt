4eae9541adb5040463cf04b2e2f3c79f435d6680
Monika Bhasin <monikabhasin.sd17@gmail.com>
2025-09-17 18:18:49 +0530

    toolbar change

--- app/src/main/java/com/example/ledgerscanner/base/ui/components/GenericToolbar.kt
+++ app/src/main/java/com/example/ledgerscanner/base/ui/components/GenericToolbar.kt
@@ -1,6 +1,8 @@
 package com.example.ledgerscanner.base.ui.components
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -11,16 +13,24 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.ledgerscanner.base.ui.theme.Grey100
+import com.example.ledgerscanner.base.ui.theme.Grey200
+import com.example.ledgerscanner.base.ui.theme.Grey500
 import com.example.ledgerscanner.base.ui.theme.White
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GenericToolbar(title: String) {
-    return TopAppBar(
-        modifier = Modifier.padding(bottom = 12.dp),
-        title = { Text(title, style = MaterialTheme.typography.headlineSmall) },
-        colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = Grey100,
+    return Column {
+        TopAppBar(
+            title = { Text(title, style = MaterialTheme.typography.headlineSmall) },
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = Grey100,
+            )
         )
-    )
+        Divider(
+            color = Grey200,  // or any color you want
+            thickness = 2.dp,
+            modifier = Modifier.padding(bottom = 12.dp),
+        )
+    }
 }
