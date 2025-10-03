5ca5e91be723d21ed46f51ab21ba43818f72c936
Monika Bhasin <monikabhasin.sd17@gmail.com>
2025-10-03 21:09:15 +0530

    CapturePreviewScree, mnavigation in compose handling

--- app/src/main/java/com/example/ledgerscanner/feature/scanner/scan/ui/compose/CapturePreviewScreen.kt
+++ app/src/main/java/com/example/ledgerscanner/feature/scanner/scan/ui/compose/CapturePreviewScreen.kt
@@ -14,17 +14,23 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat.enableEdgeToEdge
 import androidx.navigation.NavHostController
 import com.example.ledgerscanner.base.ui.components.GenericButton
 import com.example.ledgerscanner.base.ui.components.GenericToolbar
+import com.example.ledgerscanner.feature.scanner.scan.ui.dialog.WarpedImageDialog
 import com.example.ledgerscanner.feature.scanner.scan.viewmodel.OmrScannerViewModel
 
 @Composable
@@ -32,8 +38,8 @@ fun CapturePreviewScreen(
     navController: NavHostController,
     omrScannerViewModel: OmrScannerViewModel
 ) {
-
     val omrImageProcessResult by omrScannerViewModel.omrImageProcessResult.collectAsState()
+    var showDialog by remember { mutableStateOf<Boolean>(true) }
 
     Scaffold(
         modifier = Modifier
@@ -75,6 +81,15 @@ fun CapturePreviewScreen(
                     )
                 }
             }
+
+            if (showDialog)
+                WarpedImageDialog(
+                    warpedBitmap = omrImageProcessResult?.finalBitmap,
+                    intermediateBitmaps = omrImageProcessResult?.debugBitmaps,
+                    onDismiss = {
+                        showDialog = false
+                    },
+                )
         }
     }
 
